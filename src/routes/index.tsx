ac33924cc8e9c8c63b9358196b8d4efce1e74ef7
a2c <atsushi1230.sdi@gmail.com>
2022-11-12 21:07:31 +0900

    update top page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,28 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+import { getDocs, collection } from "firebase/firestore";
+import { createMemo } from "solid-js";
+import { Meta, Title } from "solid-start";
+import ArcadeStorePreviewList from "~/components/arcadeStorePreviewList/ArcadeStorePreviewList";
+import { fireStore } from "~/config/firebase";
+import ArcadeStore from "~/domain/ArcadeStore";
+
+const arcadeStores = (await getDocs(collection(fireStore, "ArcadeStores"))).docs
+  .map((value) => value.data())
+  .map(
+    (doc) =>
+      new ArcadeStore({
+        name: doc.name,
+        phoneNumber: doc.phoneNumber,
+        address: doc.address,
+      })
+  );
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>ゲーセンイキタイ</Title>
+      <h1>ゲーセンイキタイ</h1>
+      <h2>ゲーセン一覧</h2>
+      <ArcadeStorePreviewList arcadeStores={arcadeStores} />
     </main>
   );
 }
