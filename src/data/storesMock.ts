3ba62a9c36adca40d51f61393ba793a872372293
a2c <atsushi1230.sdi@gmail.com>
2022-11-12 21:08:00 +0900

    add mock data

---
+++ src/data/storesMock.ts
@@ -0,0 +1,11 @@
+import ArcadeStore from "~/domain/ArcadeStore";
+
+const storesMock: ArcadeStore[] = [
+  new ArcadeStore({
+    name: "ふじえ",
+    phoneNumber: "00011111111",
+    address: "かなざわ",
+  }),
+];
+
+export default storesMock;
