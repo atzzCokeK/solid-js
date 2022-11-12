36d7cfd077281aefbc911341725506ea1feb4fdb
a2c <atsushi1230.sdi@gmail.com>
2022-11-12 21:07:42 +0900

    update root

--- src/root.tsx
+++ src/root.tsx
@@ -16,17 +16,22 @@ import "./root.css";
 
 export default function Root() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>ゲーセンイキタイ</Title>
         <Meta charset="utf-8" />
+        <Meta
+          name="description"
+          content="Game Center Information built with Solid"
+        />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
             <A href="/">Index</A>
-            <A href="/about">About</A>
+            <A href="/about">ゲーセンイキタイとは？</A>
+            <A href="/by-game-machine">Game Machine</A>
             <Routes>
               <FileRoutes />
             </Routes>
