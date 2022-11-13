1b902ff218e5ef91eb0bc2e36077173867044bff
a2c <atsushi1230.sdi@gmail.com>
2022-11-13 10:55:34 +0900

    upate index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,30 @@
 import { getDocs, collection } from "firebase/firestore";
-import { createMemo } from "solid-js";
+import { createMemo, createResource } from "solid-js";
 import { Meta, Title } from "solid-start";
 import ArcadeStorePreviewList from "~/components/arcadeStorePreviewList/ArcadeStorePreviewList";
 import { fireStore } from "~/config/firebase";
 import ArcadeStore from "~/domain/ArcadeStore";
 
-const arcadeStores = (await getDocs(collection(fireStore, "ArcadeStores"))).docs
-  .map((value) => value.data())
-  .map(
-    (doc) =>
-      new ArcadeStore({
-        name: doc.name,
-        phoneNumber: doc.phoneNumber,
-        address: doc.address,
-      })
-  );
+const fetchArcadeStores = async () =>
+  (await getDocs(collection(fireStore, "ArcadeStores"))).docs
+    .map((value) => value.data())
+    .map(
+      (doc) =>
+        new ArcadeStore({
+          name: doc.name,
+          phoneNumber: doc.phoneNumber,
+          address: doc.address,
+        })
+    );
 
 export default function Home() {
+  const [arcadeStores] = createResource(fetchArcadeStores);
   return (
     <main>
       <Title>ゲーセンイキタイ</Title>
       <h1>ゲーセンイキタイ</h1>
       <h2>ゲーセン一覧</h2>
-      <ArcadeStorePreviewList arcadeStores={arcadeStores} />
+      <ArcadeStorePreviewList arcadeStores={arcadeStores() ?? []} />
     </main>
   );
 }
