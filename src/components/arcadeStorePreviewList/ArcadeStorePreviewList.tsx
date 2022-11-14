d7145724510c0a827d04c56cf1c4531674dd59cb
a2c <atsushi1230.sdi@gmail.com>
2022-11-14 20:25:56 +0900

    use For component

--- src/components/arcadeStorePreviewList/ArcadeStorePreviewList.tsx
+++ src/components/arcadeStorePreviewList/ArcadeStorePreviewList.tsx
@@ -1,15 +1,15 @@
-import { createMemo } from "solid-js";
+import { createMemo, For } from "solid-js";
 import ArcadeStore from "~/domain/ArcadeStore";
 import ArcadeStorePreview from "../arcadeStorePreview/ArcadeStorePreview";
 
-const ArcadeStorePreviewList = (props: { arcadeStores: ArcadeStore[] }) => {
-  const arcadeStoresView = createMemo(() => {
-    return props.arcadeStores.map((arcadeStore) =>
-      ArcadeStorePreview(arcadeStore)
-    );
-  }, [props.arcadeStores]);
-
-  return <div class="wrapper">{arcadeStoresView}</div>;
-};
+const ArcadeStorePreviewList = (props: { arcadeStores: ArcadeStore[] }) => (
+  <div class="flex bg-white p-10 rounded-lg">
+    <For each={props.arcadeStores}>
+      {(arcadeStore) => {
+        return <ArcadeStorePreview arcadeStore={arcadeStore} />;
+      }}
+    </For>
+  </div>
+);
 
 export default ArcadeStorePreviewList;
