598853f377973dd0935e822dcaed7dbd4ab734a5
a2c <atsushi1230.sdi@gmail.com>
2022-11-15 20:41:52 +0900

    update Arcade Store domain

--- src/domain/ArcadeStore.ts
+++ src/domain/ArcadeStore.ts
@@ -1,10 +1,12 @@
 import PhoneNumber from "./PhoneNumber";
+import Tag from "./Tag";
 
 type Args = {
   name: string;
   phoneNumber: string;
   address: string;
   updatedAt: Date;
+  tagIds: number[];
 };
 
 export default class ArcadeStore {
@@ -12,11 +14,13 @@ export default class ArcadeStore {
   readonly address: string;
   readonly phoneNumber: PhoneNumber;
   readonly updatedAt: Date;
+  readonly tags: Set<Tag>;
 
-  constructor({ name, phoneNumber, address, updatedAt }: Args) {
+  constructor({ name, phoneNumber, address, updatedAt, tagIds }: Args) {
     this.name = name;
     this.address = address;
     this.phoneNumber = new PhoneNumber(phoneNumber);
     this.updatedAt = updatedAt;
+    this.tags = new Set();
   }
 }
